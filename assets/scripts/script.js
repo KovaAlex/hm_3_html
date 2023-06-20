e0271129634b72de769381d113c46fe573d3e871
Kovalev Alex <k9037837817@gmail.com>
2023-06-20 12:35:58 +0300

    сделал всплывающие окно и сделал чт обы менялся цвет, нажав на кнопку

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+let name = prompt('Представьтесь, пожалуйста!');
+
+function showmsg() {
+    let msg = `Добро пожаловать на сайт, ${name}!`;
+    alert(msg);
+}
+showmsg();
+
+
+let changeColor = e => e.style.color ? e.stylr.color = " " : e.style.color = "red";
+
+
